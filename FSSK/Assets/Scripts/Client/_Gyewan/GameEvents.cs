a98109e6e5c202d02468a738d602cce712d34fa1
PerfectEgg <tamago4333@naver.com>
2026-04-29 13:49:44 +0900

    클라 #5
    쥐 코드 및 관련 이벤트 작성

--- FSSK/Assets/Scripts/Client/_Gyewan/GameEvents.cs
+++ FSSK/Assets/Scripts/Client/_Gyewan/GameEvents.cs
@@ -5,18 +5,34 @@ public static class GameEvents
 {
     // 트롤 관련 이벤트: (상태, 대상 오브젝트)
     public static Action<bool, GameObject> OnTrollInteraction;
+    public static void TriggerTrollInteraction(bool isGrabbed, GameObject obj) => OnTrollInteraction?.Invoke(isGrabbed, obj);
     
     // 아이템 관련 이벤트: (아이템 타입, 대상 오브젝트)
     public static Action<string, GameObject> OnItemCollected;
+    public static void TriggerItemCollected(string itemTag, GameObject obj) => OnItemCollected?.Invoke(itemTag, obj);
     
     // 카메라 모드 변경 이벤트: (확장 모드 여부)
     public static Action<bool> OnExpansionModeChanged;
+    public static void TriggerExpansionMode(bool isExpansion) => OnExpansionModeChanged?.Invoke(isExpansion);
+
+    // 쥐 1단계: 쥐가 매니저에게 "훔칠 돌 정보(색상, 좌표)"를 요청하는 이벤트
+    public static Action OnRequestStoneToSteal;
+    public static void TriggerRequestStoneToSteal() => OnRequestStoneToSteal?.Invoke();
+
+    // 쥐 1.5단계: 매니저가 쥐에게 정보를 전달하는 콜백 이벤트 (1: 흑돌, 2: 백돌, 좌표)
+    public static Action<int, Transform> OnStoneTargetCallback;
+    public static void TriggerStoneTargetCallback(int color, Transform pos) => OnStoneTargetCallback?.Invoke(color, pos);
+
+    // 쥐 2단계: 쥐가 목적지에 도착해서 "이 좌표의 돌을 지워줘"라고 요청하는 이벤트
+    public static Action OnExecuteSteal;
+    public static void TriggerExecuteSteal() => OnExecuteSteal?.Invoke();
 
     // 바다 게: 빈자리를 요청하는 이벤트 (반환값이 Vector3)
     public static Func<Vector3> RequestSafePosition;
 
     // 바다 게: 자리를 다 쓰고 반납하는 이벤트
     public static Action<Vector3> OnPositionReleased;
+    public static void TriggerPositionReleased(Vector3 pos) => OnPositionReleased?.Invoke(pos);
 
     // 세이렌: 매혹 동안 확장 시야 고정 이벤트
     public static Action<bool, Transform> OnSirenEffect;
@@ -30,11 +46,4 @@ public static class GameEvents
         // 구독자가 있으면 호출해서 반환받고, 아무도 없으면 (0,0,0) 반환
         return RequestSafePosition != null ? RequestSafePosition.Invoke() : Vector3.zero;
     }
-
-    
-
-    public static void TriggerTrollInteraction(bool isGrabbed, GameObject obj) => OnTrollInteraction?.Invoke(isGrabbed, obj);
-    public static void TriggerItemCollected(string itemTag, GameObject obj) => OnItemCollected?.Invoke(itemTag, obj);
-    public static void TriggerExpansionMode(bool isExpansion) => OnExpansionModeChanged?.Invoke(isExpansion);
-    public static void TriggerPositionReleased(Vector3 pos) => OnPositionReleased?.Invoke(pos);
 }
--- FSSK/Assets/Scripts/Client/_Gyewan/Troll/AnimalTroll/Rat.cs
+++ FSSK/Assets/Scripts/Client/_Gyewan/Troll/AnimalTroll/Rat.cs
@@ -3,10 +3,53 @@ using UnityEngine;
 // 쥐
 public class Rat : AnimalTroll
 {
+    [Header("쥐 설정")]
+    [SerializeField] private float _moveSpeed = 5f;
+
+
+    [Header("비주얼 (입/손에 쥐는 돌)")]
+    [SerializeField] private GameObject _heldGoldStoneVisual; // 입에 물고 있는 바둑돌 오브젝트 (평소엔 비활성화)
+    [SerializeField] private GameObject _heldSilverStoneVisual; // 입에 물고 있는 바둑돌 오브젝트 (평소엔 비활성화)
+
+    // 내부 상태 변수
+    private Vector3 _startPosition;     // 시작 위치 (훔치고 돌아가기 위해 저장)
+    private Transform _targetPosition;  // 훔칠 위치 (바둑 돌을 훔칠 위치)
+    private int _targetStoneColor;      // 바둑돌의 색깔 (0: 빈칸[활용 X], 1: 금화(흑돌), 2: 은화(백돌))
 
     void Start()
     {
         _waittingTime = 3f;
+    }
+
+    private void OnEnable()
+    {
+        // 매니저가 보내주는 타겟 정보를 수신 대기
+        GameEvents.OnStoneTargetCallback += HandleTargetAssigned;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnStoneTargetCallback -= HandleTargetAssigned;
+    }
+
+    // 매니저로부터 목표 정보를 받았을 때 호출됨
+    private void HandleTargetAssigned(int color, Transform pos)
+    {
+        // 이미 목표가 있거나 움직이는 중이면 무시
+        if (_currentState != AnimalState.Action) return;
+
+        _targetStoneColor = color;
+        _targetPosition = pos;
+        
+        transform.LookAt(_targetPosition);
+        _currentState = AnimalState.Action;
+        Debug.Log($"🐀 [도둑쥐] 목표 설정 완료: {pos} (색상: {color})");
+    }
+
+    // --- TrollBase(추상 클래스)의 메서드 구현 ---
+    public override void EndTroll() 
+    { 
+        Destroy(gameObject, 1.5f); 
     }
 
     protected override void OnStateEnter(AnimalState state)
@@ -30,6 +73,7 @@ public class Rat : AnimalTroll
                     ChangeState(AnimalState.Action);
                 break;
             case AnimalState.Action:
+                
                 break;
             case AnimalState.Exiting:
                 EndTroll();
